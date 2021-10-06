d44c072adf6489fab495e4fdda1f9f0ef26af053
psatyam360 <psatyam360@gmail.com>
2021-10-07 00:21:04 +0530

    models added

---
+++ src/app/models/Experience.ts
@@ -0,0 +1,8 @@
+export class Experience{
+    companyName?: string;
+    role?: string;
+    startDate?: Date;
+    endDate?: Date;
+    isPresent?: boolean;
+    description?: string;
+}
---
+++ src/app/models/Project.ts
@@ -0,0 +1,9 @@
+export class Project{
+    title?: string;
+    description?: string;
+    techStack?: string[];
+    startDate?: Date;
+    endDate?: Date;
+    liveURL?: string;
+    repoURL?: string;
+}
